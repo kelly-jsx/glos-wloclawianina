6d610aec2da87cc0d9e8569557f4aece96f756f1
kelly-jsx <seoch.gamer@gmail.com>
2023-04-13 15:08:09 +0200

    Carousel Component for mobile

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import { MainArticle } from './MainArticle'
+import { Carousel } from './Carousel'
 
 export const Articles = () => {
   return (
     <div className="w-full bg-base-200">
-      <h1>articles</h1>
       <MainArticle />
+      <Carousel />
     </div>
   )
 }
---
+++ src/components/Articles/Carousel.tsx
@@ -0,0 +1,65 @@
+export const Carousel = () => {
+  return (
+    <div className="carousel w-full h-28 mt-3">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="https://source.unsplash.com/random/900×700/?fruit"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+
+          <p>Article title</p>
+
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src="https://source.unsplash.com/random/900×700/?fruit"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src="https://source.unsplash.com/random/900×700/?fruit"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img
+          src="https://source.unsplash.com/random/900×700/?fruit"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
