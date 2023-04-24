40a4731a5294770f99bcdfad34c5dd11f76cdaa4
kelly-jsx <kelly-jsx@proton.me>
2023-04-24 13:41:29 +0200

    some styling for mobile

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -1,45 +1,45 @@
-import { MainArticle } from "./MainArticle";
-import { Carousel } from "./Carousel/Carousel";
+import { MainArticle } from './MainArticle'
+import { Carousel } from './Carousel/Carousel'
 
 export const Articles = () => {
   const articles = [
     {
       id: 1,
-      title: "Article 1",
-      imgUrl: "https://source.unsplash.com/random/900×700/?fruit",
-      categories: ["Category 1", "Category 2"],
+      title: 'Article 1',
+      imgUrl: 'https://source.unsplash.com/random/900×700/?fruit',
+      categories: ['Category 1', 'Category 2'],
     },
     {
       id: 2,
-      title: "Article 2",
-      imgUrl: "https://source.unsplash.com/random/900×700/?fruit",
-      categories: ["Category 1", "Category 2"],
+      title: 'Article 2',
+      imgUrl: 'https://source.unsplash.com/random/900×700/?fruit',
+      categories: ['Category 1', 'Category 2'],
     },
     {
       id: 3,
-      title: "Article 3",
-      imgUrl: "https://source.unsplash.com/random/900×700/?fruit",
-      categories: ["Category 1", "Category 2"],
+      title: 'Article 3',
+      imgUrl: 'https://source.unsplash.com/random/900×700/?fruit',
+      categories: ['Category 1', 'Category 2'],
     },
     {
       id: 4,
-      title: "Article 4",
-      imgUrl: "https://source.unsplash.com/random/900×700/?fruit",
-      categories: ["Category 1", "Category 2"],
+      title: 'Article 4',
+      imgUrl: 'https://source.unsplash.com/random/900×700/?fruit',
+      categories: ['Category 1', 'Category 2'],
     },
     {
       id: 5,
-      title: "Article 5",
-      imgUrl: "https://source.unsplash.com/random/900×700/?fruit",
-      categories: ["Category 1", "Category 2"],
+      title: 'Article 5',
+      imgUrl: 'https://source.unsplash.com/random/900×700/?fruit',
+      categories: ['Category 1', 'Category 2'],
     },
-  ];
+  ]
 
   return (
-    <div className="flex flex-col w-screen gap-3 py-4 bg-base-200">
+    <div className="flex flex-col w-screen gap-3 pb-4 bg-base-200">
       <MainArticle />
       <Carousel articles={articles} />
       <button className="mx-auto btn btn-primary">Zobacz więcej tekstów</button>
     </div>
-  );
-};
+  )
+}
--- src/components/Articles/StartpageArticles/StartpageArticles.tsx
+++ src/components/Articles/StartpageArticles/StartpageArticles.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const StartpageArticles = ({ title }: Props) => {
   return (
     <div className="p-3">
-      <div className="w-full mb-2 border-b-4 border-b-primary">
+      <div className="w-full mb-4 border-b-4 border-b-primary">
         <h1 className="text-2xl font-bold uppercase">{title}</h1>
       </div>
       <Article title="Jakieś gówno fajny artykuł" />
