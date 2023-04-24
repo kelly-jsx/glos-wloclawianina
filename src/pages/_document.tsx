fe7c48f48d9754bd4b42f96f7883062a33acf36d
kelly-jsx <kelly-jsx@proton.me>
2023-04-24 14:01:45 +0200

    Footer component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
-import { Header } from "@/components/Header";
-import { Html, Head, Main, NextScript } from "next/document";
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -9,7 +10,8 @@ export default function Document() {
         <Header />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
-  );
+  )
 }
